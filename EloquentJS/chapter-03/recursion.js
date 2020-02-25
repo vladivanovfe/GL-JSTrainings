1fdc71a7d09e2c52e122cf95dd31f588a774c4ba
vladivanovfe <vladivanovfe@gmail.com>
2020-02-25 15:30:50 +0200

    add simple validation for recursion.js

--- EloquentJS/chapter-03/recursion.js
+++ EloquentJS/chapter-03/recursion.js
@@ -1,11 +1,14 @@
 function isEven(num) {
-	if (num == 0) {
-      return true
-    } else if (num == 1) {
-      return false
-    } else if (num < 0) {
-        return isEven(-num)
-    } else {
-        return isEven(num - 2)
-    }
+    
+  if (num % 1 !== 0) {
+    console.log('Number is not const!')
+  } else if (num == 0) {
+    return true
+  } else if (num == 1) {
+    return false
+  } else if (num < 0) {
+      return isEven(-num)
+  } else {
+      return isEven(num - 2)
+  }
 }
