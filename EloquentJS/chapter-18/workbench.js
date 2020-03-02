a791440f067c36ce9e77cf880c710e2220cd7471
vladivanovfe <vladivanovfe@gmail.com>
2020-03-02 18:17:49 +0200

    update task for 18 chapter

--- EloquentJS/chapter-18/workbench.js
+++ EloquentJS/chapter-18/workbench.js
@@ -2,7 +2,7 @@ document.querySelector("#button").addEventListener("click", function() {
     let code = document.querySelector("#code").value;
     let output = document.querySelector("#output");
     try {
-      let result = Function(code);
+      let result = new Function(code)();
       output.innerText = String(result);
     } catch (e) {
       output.innerText = "Error: " + e;
