65beff6e6c42614647fdb2f9f728824c97f8d6b6
vladivanovfe <vladivanovfe@gmail.com>
2020-03-06 12:37:05 +0200

    change task for Strings

--- String/string.normalize.js
+++ String/string.normalize.js
@@ -1,5 +1,13 @@
 String.prototype.normalize = function (substr) {
-    let str = this;
-    // str.replace(  ,substr.toLowerCase());
+    let result = '';
+    let sub = String(substr).toLowerCase();
+    for (let i = 0; i < this.length; i++) {
+        if (this.indexOf(substr) > -1) {
+            result = this.substring(0, substr.length);
+        } else {
+            return new Error('No Coincidences!');
+        }
+    }
+    return this.replace(result, sub);
 }
 console.log('HELLO WORLD'.normalize('HELLO'));
