8b7bf3ea8e27e7a8ec30fa0507304cdce278f593
vladivanovfe <vladivanovfe@gmail.com>
2020-03-11 12:38:57 +0200

    add task for Strings

--- String/string.repeatSubstring.js
+++ String/string.repeatSubstring.js
@@ -1,10 +1,10 @@
 // .repeatSubstring(substring: string, times): string - Returns a given substring if is present with multiplication by times
 
 String.prototype.repeatSubstring = function (substr, times) {
-    let str = '';
+    let str = this;
     for (let i = 0; i < times; i++) {
         str += substr;
     }
     return str;
 }
-console.log(''.repeatSubstring('xaxa', 2));
+console.log('ASDF'.repeatSubstring('abc', 2));
