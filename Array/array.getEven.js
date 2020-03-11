cb1953bff577ae868274b0dc0ac3bc5aa268e39d
vladivanovfe <vladivanovfe@gmail.com>
2020-03-11 13:26:15 +0200

    add tasks for Arrays

--- Array/array.getEven.js
+++ Array/array.getEven.js
@@ -1,12 +1,12 @@
 // .getEven(): Array<number> - Returns a new array with only even numbers
 
 Array.prototype.getEven = function () {
-    return this.map(x => {
-        if (x % 2 == 0) {
-            return x;
-        } else {
-            return 'notEven';
+    let result = [];
+    this.forEach(x => {
+        if ((x % 2) == 0) {
+            result.push(x);
         }
     })
+    return result;
 };
 console.log([2,4,5,6,8,3].getEven());
--- Array/array.getOdd.js
+++ Array/array.getOdd.js
@@ -1,12 +1,12 @@
 // .getOdd(): Array<number> - Returns a new array with only odd numbers
 
 Array.prototype.getOdd = function () {
-    return this.map(x => {
+    let result = [];
+    this.forEach(x => {
         if (!(x % 2) == 0) {
-            return x;
-        } else {
-            return 'notOdd';
+            result.push(x);
         }
     })
+    return result;
 };
 console.log([2,4,5,6,8,3].getOdd());
