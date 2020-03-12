d17b6282acc7863259b46f8db742543ec3a1c8dd
vladivanovfe <vladivanovfe@gmail.com>
2020-03-12 12:43:56 +0200

    add tasks for Object

--- Object/object.createDeepCopy.js
+++ Object/object.createDeepCopy.js
@@ -1,26 +1,16 @@
 // .createDeepCopy(sourceObj: object): object - returns a deep copy of provided object. Assume that field may contain other references, they also must be deep copied. Do not use Object.assign() method.
 
-Object.prototype.createShallowCopy = function() {
-    let shallowCopy = (this instanceof Object);
-    for (let i in this) {
-        if (i == 'createShallowCopy') {
-            if (this[i] && typeof this[i] == "object") {
-                shallowCopy[i] = this[i].createShallowCopy();
-            } else {
-                shallowCopy[i] = this[i];
-            } 
-            
-            return shallowCopy;
-        };
-    };
-};
+Object.prototype.createDeepCopy = function () {
+    let copy = {};
+    let sourceObj = this;
 
-console.log({
-    name: 'Vlad',
-    age: 21,
-    smth: {
-        a: 10,
-        b: 15,
-        c: 20
-    }.createShallowCopy()
-})
+    for(key in sourceObj) {
+        if(typeof sourceObj[key] != "object" || Array) {
+            copy[key] = sourceObj[key];
+        } else {
+            copy[key] = {};
+            sourceObj[key].createDeepCopy();
+        }
+    }
+    return copy;
+}
--- Object/object.createShallowCopy.js
+++ Object/object.createShallowCopy.js
@@ -1,21 +1,12 @@
 // .createShallowCopy(sourceObj: object): object - returns "shallow" copy of provided object. The references of the first level must be copied, if there are references deeped, provide a link, not a copy.
 
-Object.prototype.createShallowCopy = function() {
-    let shallowCopy = (this instanceof Object);
-    for (let i in this) {
-        if (i == 'createShallowCopy') {
-            shallowCopy[i] = this[i];
-            return shallowCopy;
-        };
-    };
-};
+Object.prototype.createShallowCopy = function() {        
+    let copy = {};
+    let sourceObj = this;
 
-console.log({
-    name: 'Vlad',
-    age: 21,
-    smth: {
-        a: 10,
-        b: 15,
-        c: 20
-    }.createShallowCopy()
-})
+    for (let key in sourceObj) {
+        copy[key] = this[key];
+    }
+    return copy;
+
+};
